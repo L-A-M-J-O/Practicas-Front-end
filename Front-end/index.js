384ab2a556abd6d9c1b60c69a63df32cd5ad9809
L-A-M-J-O <lamjo30@gmail.com>
2022-05-12 15:48:05 -0500

    mas js

--- Front-end/index.js
+++ Front-end/index.js
@@ -1,13 +1,30 @@
 const pedir = String (prompt ('Que quieres crear'));
 
-if (pedir === 'Silla') {
-    alert ('You ready!')
-        
-        for (let Inicio = 'Separa las piezas por partes'; Inicio === 'Separa las piezas por partes';  Inicio++) {
-            alert (Inicio);
-        }
-    }
+if (pedir === 'Silla') 
+    alert ('Esta bien' )
+    let categoria = String (prompt ('Que categorias: Silla Home, Silla Gamer, Silla Office, Silla Beach'))
 
+    if (categoria == 'Silla Home' ) 
+        alert ('Comenzemos')
+            for (let Inicio = 'Separa las piezas por partes'; Inicio == 'Separa las piezas por partes';  Inicio++) 
+            alert (`Primero: ${Inicio}`);
+            for (let Inicio = 'Encuentra la base "las patas"'; Inicio == 'Encuentra la base "las patas"';  Inicio++) 
+            alert (`Segundo: ${Inicio}`);
+            for (let Inicio = 'Ubica los hoyos para poder ajustarla'; Inicio == 'Ubica los hoyos para poder ajustarla';  Inicio++) 
+            alert (`Tercero: ${Inicio}`);
+            for (let Inicio = 'Coge los tornillos y la ajustas parte por parte '; Inicio == 'Coge los tornillos y la ajustas parte por parte ';  Inicio++) 
+            alert (`Cuarto: ${Inicio}`);
+            for (let Inicio = 'Comenzamos con la parte inferior osea donde pones tu cola'; Inicio == 'Comenzamos con la parte inferior osea donde pones tu cola';  Inicio++) 
+            alert (`Quinto: ${Inicio}`);
+            for (let Inicio = 'Despues ubicamos los mango y los instalamos'; Inicio == 'Despues ubicamos los mango y los instalamos';  Inicio++) 
+            alert (`Sexto: ${Inicio}`);
+            for (let Inicio = 'Pon el espaldar lo ajustas'; Inicio == 'Pon el espaldar lo ajustas';  Inicio++) 
+            alert (`Septimo: ${Inicio}`);
+            for (let Inicio = 'Y listo a disfrutar fue sencillo'; Inicio == 'Y listo a disfrutar fue sencillo';  Inicio++) { 
+            alert (`Octavo: ${Inicio}`);
+}
+
+   
 // for  ( let Inicio = 1;  Inicio<=8; Inicio++) {
 //     console.log (Inicio);
 
