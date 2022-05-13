c909c9086bf623860e2a09bbb0ceea1f1ee6ea7d
L-A-M-J-O <lamjo30@gmail.com>
2022-05-13 17:44:45 -0500

    practica

--- Front-end/main.js
+++ Front-end/main.js
@@ -399,15 +399,18 @@
 
 // Codigo mas limpio
 
-const enemy = 'Iron man'
+// const enemy = 'Iron man'
 
-const   skin = {
-    'Iron man' : 'Magneto',
-    'Thor'  : 'Odin',
-    'Hulk' : 'Thanos',
-}
-const  skind = 'Loki'
+// const   skin = {
+//     'Iron man' : 'Magneto',
+//     'Thor'  : 'Odin',
+//     'Hulk' : 'Thanos',
+// }
+// const  skind = 'Loki'
 
-const Loki = skin [enemy] || skind 
+// const Loki = skin [enemy] || skind 
 
-console.log (Loki);
+// console.log (Loki);
+
+//  MAS PRACTICA 
+
