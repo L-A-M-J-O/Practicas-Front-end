6a619f3b022616ffc9cf4e0a2fa3d0c36379a5e9
L-A-M-J-O <lamjo30@gmail.com>
2022-06-26 10:43:09 -0500

    Diseño

--- Front-end/js/Proyecto.js
+++ Front-end/js/Proyecto.js
@@ -289,32 +289,32 @@ function agregarSuperHeroe ( ) {
 agregarSuperHeroe ( );
 
 
-function agregarSuperHeroeImagen ( ) {
-    const boton8= document.getElementById ('boton8')
-        boton8.addEventListener ('click' ,
-            ( ) => { 
-                const { value: file } = await Swal.fire({
-                    title: 'Select image',
-                    input: 'file',
-                    inputAttributes: {
-                      'accept': 'image/*',
-                      'aria-label': 'Upload your profile picture'
-                    }
-                  })
-                  if (file) {
-                    const reader = new FileReader()
-                    reader.onload = (e) => {
-                      Swal.fire({
-                        title: 'Your uploaded picture',
-                        imageUrl: e.target.result,
-                        imageAlt: 'The uploaded picture'
-                      })
-                    }
-                    reader.readAsDataURL(file)
-                }
-            })
-}
-agregarSuperHeroeImagen ( );
+// function agregarSuperHeroeImagen ( ) {
+//     const boton8= document.getElementById ('boton8')
+//         boton8.addEventListener ('click' ,
+//             ( ) => { 
+//                 const { value: file } = await Swal.fire({
+//                     title: 'Select image',
+//                     input: 'file',
+//                     inputAttributes: {
+//                       'accept': 'image/*',
+//                       'aria-label': 'Upload your profile picture'
+//                     }
+//                   })
+//                   if (file) {
+//                     const reader = new FileReader()
+//                     reader.onload = (e) => {
+//                       Swal.fire({
+//                         title: 'Your uploaded picture',
+//                         imageUrl: e.target.result,
+//                         imageAlt: 'The uploaded picture'
+//                       })
+//                     }
+//                     reader.readAsDataURL(file)
+//                 }
+//             })
+// }
+// agregarSuperHeroeImagen ( );
 
 
 
