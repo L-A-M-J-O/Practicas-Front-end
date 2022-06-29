ea401bcd00e16da431effb357c1abbae6e601387
L-A-M-J-O <lamjo30@gmail.com>
2022-06-29 02:19:51 +0200

    cambiando

--- Front-end/js/Proyecto.js
+++ Front-end/js/Proyecto.js
@@ -1,4 +1,24 @@
 
+function saludar ( )
+{
+    const nomprePedir = String ( prompt ('Dame tu nombre'));
+    const nombre = document.getElementById ('h1N');
+    nombre.innerText = ` Bienvenido ${nomprePedir}`;
+}
+// saludar ( );
+
+
+
+function preguntarN$P () 
+{
+    let pregunta0 = String (prompt ('¿Te gusta Marvel?'));
+
+    let respuesta2 = pregunta0 === 'Si' ? 'Vale sumerjete en esta aventura de conocer a tus Super Heroes' : 'Vale da te una oportunidad de conocer todo este universo'
+    alert (respuesta2);
+}
+// preguntarN$P ( );
+
+
 function SuperHeroes (a, b, c, d, h, j) 
 {
     this.estatura = a
@@ -15,13 +35,6 @@ function SuperHeroes (a, b, c, d, h, j)
     const thanosObject = new SuperHeroes ('4.10 Metros', 'Blanco', 'Masculino', '90 años', 'Inmortalidad', 'Espada')
     const hulkObject = new SuperHeroes ('4.08 Metros', 'Verde', 'Masculino', '56 años', 'Fuerza & Agresivo','No porta armas');
 
-function saludar ( )
-{
-    let nombreUsuario = String (prompt ('Tu nombre ¡por favor!'));
-    const h2= document.getElementsByTagName ('h2') [0];
-    h2.innerText = `¡Bienvenido!. ${nombreUsuario}`;  
-}
-// saludar ( );
 
 const  thor = ['Thor', '2.10 Metros', 'Dorado', '1500 años', 'Fuerza sobrehumana', 'Mjolnir'];
 const moonNigth = ['Moon Nigth', '1.80 Metros', 'Black', 'Masculino', '100 años', 'Combate cuerpo a cuerpor', 'Distintas armas como lanza dardos']
@@ -288,156 +301,11 @@ function agregarSuperHeroe ( ) {
             }
 )}
 agregarSuperHeroe ( );
-
-
-// function agregarSuperHeroeImagen ( ) {
-//     const boton8= document.getElementById ('boton8')
-//         boton8.addEventListener ('click' ,
-//             ( ) => { 
-//                 const { value: file } = await Swal.fire({
-//                     title: 'Select image',
-//                     input: 'file',
-//                     inputAttributes: {
-//                       'accept': 'image/*',
-//                       'aria-label': 'Upload your profile picture'
-//                     }
-//                   })
-//                   if (file) {
-//                     const reader = new FileReader()
-//                     reader.onload = (e) => {
-//                       Swal.fire({
-//                         title: 'Your uploaded picture',
-//                         imageUrl: e.target.result,
-//                         imageAlt: 'The uploaded picture'
-//                       })
-//                     }
-//                     reader.readAsDataURL(file)
-//                 }
-//             })
-// }
-// agregarSuperHeroeImagen ( );
-
-
-
-
-
-// La idea es que pregunte que es lo que quiere de los superheroes y despues de ello poder hacer que cambie el texto en html y luego poder cuadrat el html con imaganes y la descripcion
-
-
-// ** TRABAJO DE SILLA **//
-
-// function arraysMostrarSillas ( ) {
-//     const numeroCliente = ['Tenemos','Sillas Gaming','Silla de Verano','Silla de Casa','Moonnigth']
-//     for (let i = 0; i <=4; i++) {
-//         alert(numeroCliente [i]);
-//     }
-// }
-// arraysMostrarSillas ( );
-// function maquina ( ) {
-
-//     // ! let num0 = 'Silla Gaming'
-//    // ! let num1 = 'Silla de Verano'
-//    // ! let num2 = 'Silla de Casa'
-
-//     const sillas = String (prompt ( `¿Que vas a escoger?`));
-//         for (tipo = 0; tipo <= 10; tipo ++ ) {
-//             if (sillas === 'Silla Gaming'){
-//                 switch (tipo) {
-//                     case 1 : alert ('Primero separa tus piezas de Silla GAMING')
-//                     break;
-//                     case 2 : alert ('Segundo empieza con la base')
-//                     break;
-//                     case 3 : alert ('Tercero une la base con el cuerpo')
-//                     break;
-//                     case 4 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
-//                     break;
-//                 }
-//             }
-//             else if (sillas === 'Silla de Verano'){  
-//                 switch (tipo) {
-//                     case 1 : alert ('Primero separa tus piezas de Silla de VERANO')
-//                     break;
-//                     case 2 : alert ('Segundo empieza con la base')
-//                     break;
-//                     case 3 : alert ('Tercero une la base con el cuerpo')
-//                     break;
-//                     case 4 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
-//                     break;
-//                 }
-//             }
-//             else if (sillas === 'Silla de Casa') {
-//                 switch (tipo) {
-//                     case 1 : alert ('Primero separa tus piezas de Silla de CASA')
-//                     break;
-//                     case 2 : alert ('Segundo empieza con la base')
-//                     break;
-//                     case 3 : alert ('Tercero une la base con el cuerpo')
-//                     break;
-//                     case 4 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
-//                     break;
-//                 }
-//             }
-//             else if (sillas === 'Moon') {
-//                 switch (tipo) {
-//                     case 1 : alert (`Este es MoonNigth Estatura ${moonNigth.estatura} , Color Representativo ${moonNigth.color} ,  Edad ${moonNigth.edad} ,  Genero ${moonNigth.genero}`); 
-//                     break;
-//                     case 2 : alert (`Sus enemigos son ${enemyMoon}`)
-//                     break;
-//                 }  
-//             }
-//         }
-//         function despedida ( ) 
-//         {
-//             let preguntaDeDespedida = String (prompt ('¿Quedaste Satisfecho?'));
-//             for ( despedida = 0; despedida <= 10; despedida ++) 
-//             {
-//                 if (preguntaDeDespedida === 'Si') {
-//                     alert (`Fue un gusto ${nombreUsuario}`)
-//                 }
-//                 else if (preguntaDeDespedida === 'No') {    
-//                     function info ( )
-//                     {
-//                         let pregunta1 = prompt ('Esta bien indicame que parte, 1  2  3  4  exit')
-//                         for (pasos = 0; pasos <=5; pasos++)
-//                         {
-//                             if (pregunta1 === '1') {
-//                                 switch (pasos) 
-//                                 {
-//                                     case 1 : alert ('Primero separa tus piezas de Silla')
-//                                     break;
-//                                 }
-//                             }
-//                             else if (pregunta1 === '2') {
-//                                 switch (pasos) 
-//                                 {
-//                                     case 1 : alert ('Segundo empieza con la base')
-//                                     break;
-//                                 }
-//                             }
-//                             else if (pregunta1 === '3') {
-//                                 switch (pasos) 
-//                                 {
-//                                     case 1 : alert ('Tercero une la base con el cuerpo')
-//                                     break;
-//                                 }
-//                             }
-//                             else if (pregunta1 === '4') {
-//                                 switch (pasos) 
-//                                 {
-//                                     case 1 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
-//                                     break;
-//                                 }
-//                             }
-//                             else if (pregunta1=== 'exit') {
-//                                 alert (`Fue un gusto ${nombreUsuario}`);
-//                             }
-//                         }
-//                     }
-//                     info ();
-//                 }
-//             }
-//     }
-//     despedida ( );
-// }
-// maquina ( );
+function agregarSuperHeroeImagen ( ) {
+    const boton8= document.getElementById ('boton8')
+        boton8.addEventListener ('click' ,
+            ( ) => {
+                
+            })}
+agregarSuperHeroeImagen ( );
 
--- Front-end/js/main.js
+++ Front-end/js/main.js
@@ -839,3 +839,121 @@ localStorage.setItem ('Usuario', JSON.stringify(usario));
 
 console.log (JSON.parse(localStorage.getItem ('Usuario')));
 
+
+// ** TRABAJO DE SILLA **//
+
+// function arraysMostrarSillas ( ) {
+//     const numeroCliente = ['Tenemos','Sillas Gaming','Silla de Verano','Silla de Casa','Moonnigth']
+//     for (let i = 0; i <=4; i++) {
+//         alert(numeroCliente [i]);
+//     }
+// }
+// arraysMostrarSillas ( );
+// function maquina ( ) {
+
+//     // ! let num0 = 'Silla Gaming'
+//    // ! let num1 = 'Silla de Verano'
+//    // ! let num2 = 'Silla de Casa'
+
+//     const sillas = String (prompt ( `¿Que vas a escoger?`));
+//         for (tipo = 0; tipo <= 10; tipo ++ ) {
+//             if (sillas === 'Silla Gaming'){
+//                 switch (tipo) {
+//                     case 1 : alert ('Primero separa tus piezas de Silla GAMING')
+//                     break;
+//                     case 2 : alert ('Segundo empieza con la base')
+//                     break;
+//                     case 3 : alert ('Tercero une la base con el cuerpo')
+//                     break;
+//                     case 4 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
+//                     break;
+//                 }
+//             }
+//             else if (sillas === 'Silla de Verano'){  
+//                 switch (tipo) {
+//                     case 1 : alert ('Primero separa tus piezas de Silla de VERANO')
+//                     break;
+//                     case 2 : alert ('Segundo empieza con la base')
+//                     break;
+//                     case 3 : alert ('Tercero une la base con el cuerpo')
+//                     break;
+//                     case 4 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
+//                     break;
+//                 }
+//             }
+//             else if (sillas === 'Silla de Casa') {
+//                 switch (tipo) {
+//                     case 1 : alert ('Primero separa tus piezas de Silla de CASA')
+//                     break;
+//                     case 2 : alert ('Segundo empieza con la base')
+//                     break;
+//                     case 3 : alert ('Tercero une la base con el cuerpo')
+//                     break;
+//                     case 4 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
+//                     break;
+//                 }
+//             }
+//             else if (sillas === 'Moon') {
+//                 switch (tipo) {
+//                     case 1 : alert (`Este es MoonNigth Estatura ${moonNigth.estatura} , Color Representativo ${moonNigth.color} ,  Edad ${moonNigth.edad} ,  Genero ${moonNigth.genero}`); 
+//                     break;
+//                     case 2 : alert (`Sus enemigos son ${enemyMoon}`)
+//                     break;
+//                 }  
+//             }
+//         }
+//         function despedida ( ) 
+//         {
+//             let preguntaDeDespedida = String (prompt ('¿Quedaste Satisfecho?'));
+//             for ( despedida = 0; despedida <= 10; despedida ++) 
+//             {
+//                 if (preguntaDeDespedida === 'Si') {
+//                     alert (`Fue un gusto ${nombreUsuario}`)
+//                 }
+//                 else if (preguntaDeDespedida === 'No') {    
+//                     function info ( )
+//                     {
+//                         let pregunta1 = prompt ('Esta bien indicame que parte, 1  2  3  4  exit')
+//                         for (pasos = 0; pasos <=5; pasos++)
+//                         {
+//                             if (pregunta1 === '1') {
+//                                 switch (pasos) 
+//                                 {
+//                                     case 1 : alert ('Primero separa tus piezas de Silla')
+//                                     break;
+//                                 }
+//                             }
+//                             else if (pregunta1 === '2') {
+//                                 switch (pasos) 
+//                                 {
+//                                     case 1 : alert ('Segundo empieza con la base')
+//                                     break;
+//                                 }
+//                             }
+//                             else if (pregunta1 === '3') {
+//                                 switch (pasos) 
+//                                 {
+//                                     case 1 : alert ('Tercero une la base con el cuerpo')
+//                                     break;
+//                                 }
+//                             }
+//                             else if (pregunta1 === '4') {
+//                                 switch (pasos) 
+//                                 {
+//                                     case 1 : alert ('Cuarto y ultimo solo te quedaria unir el espaldar y mangos')
+//                                     break;
+//                                 }
+//                             }
+//                             else if (pregunta1=== 'exit') {
+//                                 alert (`Fue un gusto ${nombreUsuario}`);
+//                             }
+//                         }
+//                     }
+//                     info ();
+//                 }
+//             }
+//     }
+//     despedida ( );
+// }
+// maquina ( );
+
