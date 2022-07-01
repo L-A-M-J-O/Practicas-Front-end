a55c8efe66d503f474eed6f80b6092c641ffcecc
L-A-M-J-O <lamjo30@gmail.com>
2022-07-01 13:01:03 +0200

    F

--- Front-end/js/pruebas.js
+++ Front-end/js/pruebas.js
@@ -1,39 +1,39 @@
-// programaAcciones ( )
+programaAcciones ( )
 
-// function programaAcciones ( )
-// {
-//   programaBotonVer ( )
-// }
+function programaAcciones ( )
+{
+  programaBotonVer ( )
+}
 
-// function programaBotonVer ( )
-// {
-//     const btn = document.getElementById ('ver')
-//     btn.addEventListener ('click', ( )=> 
-//     {
-//         cargarListadoDePosts ( );
-//     })
-// }
+function programaBotonVer ( )
+{
+    const btn = document.getElementById ('ver')
+    btn.addEventListener ('click', ( )=> 
+    {
+        cargarListadoDePosts ( );
+    })
+}
 
-// function cargarListadoDePosts ( )
-// {
-//   fetch('https://jsonplaceholder.typicode.com/posts/1')
-//   .then((res)=>res.json ( ))
-//   .then((json)=>mostrarListadoDePosts(json))
-// }
+function cargarListadoDePosts ( )
+{
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((res)=>res.json ( ))
+  .then((json)=>mostrarListadoDePosts(json))
+}
 
-// function mostrarListadoDePosts (posts)
-// {
-//   const container = document.getElementById ('appContainer')
-//   posts.forEach((ver)=>{
-//     mostrarPosts (ver, container)
-//   })
-// }
+function mostrarListadoDePosts (posts)
+{
+  const container = document.getElementById ('appContainer')
+  posts.forEach((ver)=>{
+    mostrarPosts (ver, container)
+  })
+}
 
-// function mostrarPosts (post, div)
-// {
-//   const divPost= document.createElement('div')
-//   divPost.innerHTML= `<h2>${post.title}</h2>
-//                                     <p>${post.body}</p>
-//                                     <hr>`
-//   div.appendChild(divPost);
-// }
+function mostrarPosts (post, div)
+{
+  const divPost= document.createElement('div')
+  divPost.innerHTML= `<h2>${post.title}</h2>
+                                    <p>${post.body}</p>
+                                    <hr>`
+  div.appendChild(divPost);
+}
