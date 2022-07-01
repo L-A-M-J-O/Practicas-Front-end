4b0c6cce69c15127997808dca987432450c32e17
L-A-M-J-O <lamjo30@gmail.com>
2022-07-01 02:59:27 +0200

    Tratando de consumir una api pero no deja

--- Front-end/js/pruebas.js
+++ Front-end/js/pruebas.js
@@ -1,47 +1,39 @@
-const array = [1,2,3,4,5];
+// programaAcciones ( )
 
-array.forEach(function ( element ){
-  console.log (element)
-});
+// function programaAcciones ( )
+// {
+//   programaBotonVer ( )
+// }
 
+// function programaBotonVer ( )
+// {
+//     const btn = document.getElementById ('ver')
+//     btn.addEventListener ('click', ( )=> 
+//     {
+//         cargarListadoDePosts ( );
+//     })
+// }
 
-programaAcciones ( )
+// function cargarListadoDePosts ( )
+// {
+//   fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then((res)=>res.json ( ))
+//   .then((json)=>mostrarListadoDePosts(json))
+// }
 
-function programaAcciones ( )
-{
-  programaBotonVer ( )
-}
+// function mostrarListadoDePosts (posts)
+// {
+//   const container = document.getElementById ('appContainer')
+//   posts.forEach((ver)=>{
+//     mostrarPosts (ver, container)
+//   })
+// }
 
-function programaBotonVer ( )
-{
-    const btn = document.getElementById ('ver')
-    btn.addEventListener ('click', ( )=> 
-    {
-        cargarListadoDePosts ( );
-    })
-}
-
-function cargarListadoDePosts ( )
-{
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((res)=>res.json ( ))
-  .then((json)=>mostrarListadoDePosts(json))
-}
-
-function mostrarListadoDePosts (posts)
-{
-  const container = document.getElementById ('appContainer')
-  posts.forEach((ver)=>{
-    mostrarPosts (ver, container)
-  })
-}
-
-function mostrarPosts (post, div)
-{
-  const divPost= document.createElement('div')
-  divPost.innerHTML= `<h2>${post.title}</h2>
-                                    <p>${post.body}</p>
-                                    <hr>`
-  div.appendChild(divPost);
-}
-
+// function mostrarPosts (post, div)
+// {
+//   const divPost= document.createElement('div')
+//   divPost.innerHTML= `<h2>${post.title}</h2>
+//                                     <p>${post.body}</p>
+//                                     <hr>`
+//   div.appendChild(divPost);
+// }
